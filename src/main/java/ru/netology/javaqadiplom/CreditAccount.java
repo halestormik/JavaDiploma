e13f8a1691746dfcd9a0c6c5b13b3d1937d746bb
Oleg Shestakov <eithufter@gmail.com>
2024-01-10 17:32:07 +0300

    fix rate

--- src/main/java/ru/netology/javaqadiplom/CreditAccount.java
+++ src/main/java/ru/netology/javaqadiplom/CreditAccount.java
@@ -101,7 +101,7 @@ public class CreditAccount extends Account {
         if (balance > 0) {
             return 0;
         } else {
-            return balance / 100 * rate;
+            return balance * rate / 100;
         }
     }
 
